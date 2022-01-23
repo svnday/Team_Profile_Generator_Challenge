e242e41cdc67d08be83e9c1384db2ed220dfd9cb
svnday <40030806+svnday@users.noreply.github.com>
2022-01-23 14:18:51 -0600

    switching to start tests from a different role. Will do employee last

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,3 @@
 const Employee = require('../lib/Employee.js');
 
-test('gets employee info', () => {
-    const employee = new Employee('Julian')
-
-    expect(employee.name).toBe('Julian');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe(expect.any(String));
-
 })
