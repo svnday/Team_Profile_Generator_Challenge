caf3fc41c6c0b800b74b9f3b3bf0ee29a3810d00
svnday <40030806+svnday@users.noreply.github.com>
2022-01-23 20:50:00 -0600

    manager tests failed, then passed. Manager methods operational

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
-const Manager = require("./lib/Manager");
+
+const Manager = require("../lib/Manager");
+
+test('get office number', () => {
+    const testOffice = 15;
+    const employee = new Manager('name', '5', 'email@email.com', testOffice);
+    expect(employee.officeNumber).toBe(testOffice);
+});
+
+test('get role', () => {
+    const testRole = 'Manager';
+    const employee = new Manager('name', '5', 'email@gmail.com', 15);
+    expect(employee.getRole()).toBe(testRole);
+});
